08daafa6dc8ecdf90fdef761f3b442fb8b072cd6
Loki2942 <125013031+Loki2942@users.noreply.github.com>
2023-12-08 18:31:24 +0400

    Update Lab_13.py

--- Lab_13.py
+++ Lab_13.py
@@ -4,27 +4,40 @@
 import csv
 
 file = open("titanic.csv", "r")
-reader = list(csv.reader(file))
+rows = list(csv.reader(file))
 
 # Переменные для подсчета
 counter_alive = 0
 counter_total = 0
 sum = 0
 num = 0
+age_list = []  # список возрастов
 
-for i in reader:
+for i in rows:
+    if i[1] == "1" or i[1] == "0":  # для обхода первой строки
+        if i[5] != "" and i[5] != " ":  # Проверка на пустую строку
+            if float(i[5]) >= 18.0:
+                age_list.append(i[5])  # добавляем в список возрастов
+
+for i in rows:
     if i[1] == "1" or i[1] == "0":  # для обхода первой строки
         if i[5] != "" and i[5] != " ":  # Проверка на пустую строку
             sum += float(i[5])
             num += 1
         average_age = sum / num  # средний возраст
 
+age_list.append(str((average_age)))
+sort_aglist = sorted(age_list)
+indx = sort_aglist.index(str((average_age)))
+average_age_list = sort_aglist[
+                   indx - 5: indx + 6]  # Берем срез значений из списка +- 5 позиций, считая от индекса среднего возраста
+
+for i in rows:
     if i[11] == "C":  # Проверка на соответствие условию C = Cherbourg Q = Queenstown S = Southampton
         if i[5] != "" and i[5] != " ":  # Проверка на пустую строку
-            if float(i[5]) >= 18.0:
-                if float(i[5]) > average_age - 5 or float(i[5]) < average_age + 5:
-                    counter_total += 1
-                    if i[1] == "1":
-                        counter_alive += 1
+            if i[5] in average_age_list:
+                counter_total += 1
+                if i[1] == "1":
+                    counter_alive += 1
 
 print(f"Взрослых, севших в порту Шейбур : {counter_total}\nИз них выжило: {counter_alive}")
